af7266a440f8094aa915e3f05f752a7d42537e7f
Hakeem Adam <tuffassassin11@gmail.com>
2024-10-28 20:24:17 +0100

    Update SimpleBox.cpp
    added menu and different types

--- SimpleBox.cpp
+++ SimpleBox.cpp
@@ -1,3 +1,5 @@
+#include "SimpleBox.h"
+
 #include <UT/UT_DSOVersion.h>
 #include "SimpleBox.h"
 
@@ -9,8 +11,22 @@
 
 
 #include <PRM/PRM_Include.h>
+#include  <PRM/PRM_Default.h>
+#include <PRM/PRM_Template.h>
+//#include <PRM/PRM_Shared.h>
 
 
+
+
+static PRM_Name boxTypeChoice[]=
+{
+    PRM_Name("polyline", "Polyline"),
+    PRM_Name("polygon", "Polygon"),
+    PRM_Name("primitive", "Primitive"),
+    PRM_Name(0)
+};
+
+static PRM_ChoiceList typeMenu(PRM_CHOICELIST_SINGLE , boxTypeChoice);
 
 
 static PRM_Name names[]=
@@ -19,7 +35,9 @@ static PRM_Name names[]=
     PRM_Name("rotate", "Rotate"),
     PRM_Name("center", "Center"),
     PRM_Name("uniScale", "Uniform Scale"),
-    PRM_Name("axis_division", "Axis Division")
+    PRM_Name("axis_division", "Axis Division"),
+    PRM_Name("boxtype", "Box Type"),
+    PRM_Name(0)
 };
 
 static PRM_Default defaults[]=
@@ -30,8 +48,11 @@ static PRM_Default defaults[]=
     PRM_Default(0.0f),
     PRM_Default(0.0f),
     PRM_Default(0.0f),
-    PRM_Default(2.0f)
+    PRM_Default(2.0f),
+    
 };
+
+static PRM_Default boxTypeDefault(0);
 
 static PRM_Template myTemplateList[] =
     {
@@ -40,6 +61,7 @@ static PRM_Template myTemplateList[] =
     PRM_Template(PRM_XYZ, 3, &names[2], defaults+3),
     PRM_Template(PRM_FLT, 1, &names[3], &defaults[0]),
     PRM_Template(PRM_INT, 1, &names[4], &defaults[6]),
+    PRM_Template(PRM_ORD, 1, &names[5], 0, &typeMenu), 
     PRM_Template()
     };
 
@@ -88,9 +110,24 @@ OP_ERROR SimpleBox::cookMySop(OP_Context& context)
     float zmax = evalFloat("size", 2, context.getTime())/2.0f;
 
     int div = evalInt("axis_division", 0, context.getTime());
+
+    int boxType = evalInt("boxtype", 0, context.getTime());
+
+    if(boxType ==0)
+    {
+        gdp->cube(xmin, xmax, ymin, ymax, zmin, zmax, div, div, div, 0,1);
+    }else
+        if (boxType ==1)
+    {
+            gdp->meshCube(div,div,div,xmin, xmax,ymin,ymax,zmin,zmax,GEO_PATCH_QUADS,true);
     
+    } else
+        if(boxType==2)
+    {
+        gdp->polymeshCube(div,div,div,xmin,xmax,ymin,ymax,zmin,zmax,GEO_PATCH_QUADS,true);
+    }
     //gdp->cube(xmin, xmax, ymin, ymax, zmin, zmax, div, div, div, 0,1);
-    gdp->meshCube(div,div,div,xmin, xmax,ymin,ymax,zmin,zmax,GEO_PATCH_QUADS,true);
+    //gdp->meshCube(div,div,div,xmin, xmax,ymin,ymax,zmin,zmax,GEO_PATCH_QUADS,true);
     
     return error();
 }
